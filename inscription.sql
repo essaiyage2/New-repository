596601912b789e4895189e9604d0dc8ebab4da34
Jamesfkj <124376782+Jamesfkj@users.noreply.github.com>
2024-01-07 23:31:17 +0000

    Update inscription.sql

--- inscription.sql
+++ inscription.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE inscription
 
 CREATE TABLE compte (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_icnrtuyrejhr
     username VARCHAR(8) NOT NULL,
     password VARCHAR(16) NOT NULL,
     ieunecbncm,ni;cl
